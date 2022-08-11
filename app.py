01ca87f8b83a033b6f7209a61cde3fda6553c219
ZidaneBaraba <mzidane49@gmail.com>
2022-08-11 15:27:48 +0700

    edit app py

--- app.py
+++ app.py
@@ -41,32 +41,7 @@ def pred():
         df['prediction'] = prediction
         # df['prediction'] = df['prediction'].replace(['0','1'],['Tidak ada penyakit Diabetes','Kemungkinan Terkena Diabetes. Silahkan Hubungi dokter untuk memastikan'])
         return render_template('result.html', pred=str(df['prediction'][0]), name='name')
-
-@app.route('/predict_api', methods=['GET','POST'])
-def pred():
-    df = pd.DataFrame(columns=['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age'])
     
-    if request.method == "POST":
-        Pregnancies = request.form["Pregnancies"]
-        Glucose = request.form["Glucose"]
-        BloodPressure = request.form["BloodPressure"]
-        SkinThickness = request.form["SkinThickness"]
-        Insulin = request.form["Insulin"]
-        BMI = request.form["BMI"]
-        DiabetesPedigreeFunction = request.form["DiabetesPedigreeFunction"]
-        Age = request.form["Age"]
-    
-        df = df.append({'Pregnancies': Pregnancies, 'Glucose': Glucose, 'BloodPressure': BloodPressure, 'SkinThickness': SkinThickness,'Insulin': Insulin, 'BMI': BMI, 'DiabetesPedigreeFunction': DiabetesPedigreeFunction, 'Age': Age}, ignore_index=True)
-        
-        cc = sc.transform(df)
-        
-        with open('./model/randomforest_old_method.pkl','rb') as file:
-            rf = pickle.load(file)
-
-        prediction = rf.predict(cc)
-        df['prediction'] = prediction
-        # df['prediction'] = df['prediction'].replace(['0','1'],['Tidak ada penyakit Diabetes','Kemungkinan Terkena Diabetes. Silahkan Hubungi dokter untuk memastikan'])
-        return df['prediction'][0]   
     
 
 if __name__ == "__main__":
